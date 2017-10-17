a6c43e9e7bb8e7fba366361af6560b6ef8c54d07
ThomasSINNO <thomas.sinno@orange.fr>
2017-10-17 11:38:59 +0200

    moving + jump

--- Assets/Scripts/character.cs
+++ Assets/Scripts/character.cs
@@ -7,85 +7,304 @@ public class character : MonoBehaviour {
     private float height, width;
     public float distance;
     bool dessus, dessous, gauche, droite;
+    public bool landed;
+    float VerticalSpeed;
+    public float g;
+    public float jumpPower;
     float time;
-    public float gpower;
 
-    public void jump()
+
+
+    public float CollisionLeft()
     {
+        RaycastHit2D hit1 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, height - 0.1f, 0), new Vector3(-1, 0, 0));
+        RaycastHit2D hit2 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, 0, 0), new Vector3(-1, 0, 0));
+        RaycastHit2D hit3 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, -height + 0.1f, 0), new Vector3(-1, 0, 0));
+
+        Debug.DrawRay(transform.position + new Vector3(-width, height, 0), new Vector3(-1, 0, 0));
+        Debug.DrawRay(transform.position + new Vector3(-width, 0, 0), new Vector3(-1, 0, 0));
+        Debug.DrawRay(transform.position + new Vector3(-width, -height, 0), new Vector3(-1, 0, 0));
+
+        float val1 = new float(), val2 = new float(), val3 = new float();
+        val1 = -100; val2 = -100; val3 = -100;
+
+        if (hit1)
+        {
+
+            val1 = hit1.point.x;
+            //print("detect 1 !");
+        }
+        if (hit2)
+        {
+
+            val2 = hit2.point.x;
+            //print("detect 2 !");
+        }
+        if (hit3)
+        {
+
+            val3 = hit3.point.x;
+            //print("detect 3 !");
+        }
+
+        float minX = Mathf.Max(val1, val2, val3) + width + 0.35f;
+
+        return minX;
+    }
+
+    public float CollisionRight()
+    {
+        RaycastHit2D hit4 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, height - 0.1f, 0), new Vector3(1, 0, 0));
+        RaycastHit2D hit5 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, 0, 0), new Vector3(1, 0, 0));
+        RaycastHit2D hit6 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, -height + 0.1f, 0), new Vector3(1, 0, 0));
+
+        Debug.DrawRay(transform.position + new Vector3(width, height, 0), new Vector3(1, 0, 0));
+        Debug.DrawRay(transform.position + new Vector3(width, 0, 0), new Vector3(1, 0, 0));
+        Debug.DrawRay(transform.position + new Vector3(width, -height, 0), new Vector3(1, 0, 0));
+
+        float val4 = new float(), val5 = new float(), val6 = new float();
+        val4 = 100; val5 = 100; val6 = 100;
+
+        if (hit4)
+        {
+
+            val4 = hit4.point.x;
+            //print("detect 4 !");
+        }
+        if (hit5)
+        {
+
+            val5 = hit5.point.x;
+            //print("detect 5 !");
+        }
+        if (hit6)
+        {
+            val6 = hit6.point.x;
+            //print("detect 6 !");
+        }
+
+        float maxX = Mathf.Min(val4, val5, val6) - width - 0.35f;
+
+        return maxX;
+    }
+
+    public float CollisionUp()
+    {
+        RaycastHit2D hit10 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, height - 0.1f, 0), new Vector3(0, 1, 0));
+        RaycastHit2D hit11 = Physics2D.Raycast(transform.position + new Vector3(0, height - 0.1f, 0), new Vector3(0, 1, 0));
+        RaycastHit2D hit12 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, height - 0.1f, 0), new Vector3(0, 1, 0));
+
+        Debug.DrawRay(transform.position + new Vector3(width, height, 0), new Vector3(0, 1, 0));
+        Debug.DrawRay(transform.position + new Vector3(0, height, 0), new Vector3(0, 1, 0));
+        Debug.DrawRay(transform.position + new Vector3(-width, height, 0), new Vector3(0, 1, 0));
+
+        float val10 = new float(), val11 = new float(), val12 = new float();
+        val10 = 100; val11 = 100; val12 = 100;
+
+        if (hit10)
+        {
+            val10 = hit10.point.y;
+            //print("detect 10 !");
+        }
+        if (hit11)
+        {
+            val11 = hit11.point.y;
+            //print("detect 11 !");
+        }
+        if (hit12)
+        {
+            val12 = hit12.point.y;
+            //print("detect 12 !");
+        }
+
+        float maxY = Mathf.Min(val10, val11, val12) - height - 0.35f;
+
+        return maxY;
+    }
+
+    public float CollisionDown()
+    {
+        RaycastHit2D hit7 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, -height + 0.1f, 0), new Vector3(0, -1, 0));
+        RaycastHit2D hit8 = Physics2D.Raycast(transform.position + new Vector3(0, -height + 0.1f, 0), new Vector3(0, -1, 0));
+        RaycastHit2D hit9 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, -height + 0.1f, 0), new Vector3(0, -1, 0));
+
+        Debug.DrawRay(transform.position + new Vector3(width, -height, 0), new Vector3(0, -1, 0));
+        Debug.DrawRay(transform.position + new Vector3(0, -height, 0), new Vector3(0, -1, 0));
+        Debug.DrawRay(transform.position + new Vector3(-width, -height, 0), new Vector3(0, -1, 0));
+
+        float val7 = new float(), val8 = new float(), val9 = new float();
+        val7 = -100; val8 = -100; val9 = -100;
+
+        if (hit7)
+        {
+            val7 = hit7.point.y;
+            //print("detect 7 !");
+        }
+        if (hit8)
+        {
+            val8 = hit8.point.y;
+            //print("detect 8 !");
+        }
+        if (hit9)
+        {
+            val9 = hit9.point.y;
+            //print("detect 9 !");
+        }
+
+        float minY = Mathf.Max(val7, val8, val9) + height + 0.35f;
+
+        if (this.gameObject.transform.position.y - minY < 0.02)
+        {
+            landed = true;
+            print("landed true");
+            
+        } else
+        {
+            landed = false;
+        }
+        return minY;
         
     }
 
-    private void OnCollisionEnter2D(Collision2D collision)
-    {
-        float x1 = this.gameObject.GetComponent<Collider2D>().bounds.size.x;
-        float x2 = collision.gameObject.GetComponent<Collider2D>().bounds.size.x;
-        float y1 = this.gameObject.GetComponent<Collider2D>().bounds.size.y;
-        float y2 = collision.gameObject.GetComponent<Collider2D>().bounds.size.y;
-
-        Vector2 pos1 = this.transform.position;
-        Vector2 pos2 = this.transform.position;
-
-        dessus = (pos1.y > pos2.y)&&(Mathf.Abs(pos1.x-pos2.x)<Mathf.Abs(x1-x2));
-        dessous = (pos1.y < pos2.y)&& (Mathf.Abs(pos1.x - pos2.x) < Mathf.Abs(x1 - x2));
-        gauche = (pos1.x < pos2.x)&& (Mathf.Abs(pos1.y - pos2.y) < Mathf.Abs(y1 - y2));
-        droite = (pos1.x > pos2.x) && (Mathf.Abs(pos1.y - pos2.y) < Mathf.Abs(y1 - y2)); ;
+    //void collisionhandler()
+    //{
 
 
-        if (dessus)
-        {
+    //    RaycastHit2D hit1 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, height - 0.1f, 0), new Vector3(-1, 0, 0));
+    //    RaycastHit2D hit2 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, 0, 0), new Vector3(-1, 0, 0));
+    //    RaycastHit2D hit3 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, -height + 0.1f, 0), new Vector3(-1, 0, 0));
 
-            this.transform.position = new Vector3(this.transform.position.x,pos2.y + y2+y1,this.transform.position.z);
-            print("dessus");
-        }
-        else if (dessous)
-        {
-            this.transform.position = new Vector3(this.transform.position.x, pos2.y - y2 -y1, this.transform.position.z);
-            print("dessous");
-        }
-        else if (gauche)
-        {
-            this.transform.position = new Vector3( pos2.x - x2 -x1,this.transform.position.x, this.transform.position.z);
-            print("gauche");
-        }
-        else if (droite)
-        {
-            this.transform.position = new Vector3(pos2.x + x2 +x1, this.transform.position.x, this.transform.position.z);
-            print("droite");
-        }
+    //    RaycastHit2D hit4 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, height - 0.1f, 0), new Vector3(1, 0, 0));
+    //    RaycastHit2D hit5 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, 0 , 0), new Vector3(1, 0, 0));
+    //    RaycastHit2D hit6 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, -height + 0.1f, 0), new Vector3(1, 0, 0));
 
-        print("OnCollisionEnter");
-    }
+    //    RaycastHit2D hit7 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, -height + 0.1f, 0), new Vector3(0, -1, 0));
+    //    RaycastHit2D hit8 = Physics2D.Raycast(transform.position + new Vector3(0, -height + 0.1f, 0), new Vector3(0, -1, 0));
+    //    RaycastHit2D hit9 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, -height + 0.1f, 0), new Vector3(0, -1, 0));
 
-    void collisionhandler()
-    {
-        bool ray1 = Physics.Raycast(transform.position + new Vector3(-width,height,0), new Vector3(-1, 0, 0), distance);
-        bool ray2 = Physics.Raycast(transform.position + new Vector3(-width, 0, 0), new Vector3(-1, 0, 0), distance);
-        bool ray3 = Physics.Raycast(transform.position + new Vector3(-width, -height, 0), new Vector3(-1, 0, 0), distance);
+    //    RaycastHit2D hit10 = Physics2D.Raycast(transform.position + new Vector3(width - 0.1f, height - 0.1f, 0), new Vector3(0, 1, 0));
+    //    RaycastHit2D hit11 = Physics2D.Raycast(transform.position + new Vector3(0 , height - 0.1f, 0), new Vector3(0, 1, 0));
+    //    RaycastHit2D hit12 = Physics2D.Raycast(transform.position + new Vector3(-width + 0.1f, height - 0.1f, 0), new Vector3(0, 1, 0));
 
-        bool ray4 = Physics.Raycast(transform.position + new Vector3(width, height, 0), new Vector3(1, 0, 0), distance);
-        bool ray5 = Physics.Raycast(transform.position + new Vector3(width, 0, 0), new Vector3(1, 0, 0), distance);
-        bool ray6 = Physics.Raycast(transform.position + new Vector3(width, -height, 0), new Vector3(1, 0, 0), distance);
+    //    Debug.DrawRay(transform.position + new Vector3(-width, height, 0), new Vector3(-1, 0, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(-width, 0, 0), new Vector3(-1, 0, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(-width, -height, 0), new Vector3(-1, 0, 0));
 
-        bool ray7 = Physics.Raycast(transform.position + new Vector3(width, -height, 0), new Vector3(0, -1, 0), distance);
-        bool ray8 = Physics.Raycast(transform.position + new Vector3(0, -height, 0), new Vector3(0, -1, 0), distance);
-        bool ray9 = Physics.Raycast(transform.position + new Vector3(-width, -height, 0), new Vector3(0, -1, 0), distance);
+    //    Debug.DrawRay(transform.position + new Vector3(width, height, 0), new Vector3(1, 0, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(width, 0, 0), new Vector3(1, 0, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(width, -height, 0), new Vector3(1, 0, 0));
 
-        if (ray1 || ray2 || ray3)
-        {
-            this.transform.position = new Vector3()
-        }
+    //    Debug.DrawRay(transform.position + new Vector3(width, -height, 0), new Vector3(0, -1, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(0, -height, 0), new Vector3(0, -1, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(-width, -height, 0), new Vector3(0, -1, 0));
 
-    }
+    //    Debug.DrawRay(transform.position + new Vector3(width, height, 0), new Vector3(0, 1, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(0, height, 0), new Vector3(0, 1, 0));
+    //    Debug.DrawRay(transform.position + new Vector3(-width, height, 0), new Vector3(0, 1, 0));
 
-    private void OnCollisionExit2D(Collision2D collision)
-    {
-        dessus = false;
-        dessous = false;
-        gauche = false;
-        droite = false;
-    }
+    //    float   val1 = new float(), val2 = new float(), val3 = new float(), 
+    //            val4 = new float(), val5 = new float(), val6 = new float(),
+    //            val7 = new float(), val8 = new float(), val9 = new float(), 
+    //            val10 = new float(), val11 = new float(), val12 = new float();
+
+    //    val1 = -100; val2 = -100; val3 = -100;
+    //    val4 = 100; val5 = 100; val6 = 100;
+    //    val7 = -100; val8 = -100; val9 = -100;
+    //    val10 = 100; val11 = 100; val12 = 100;
 
 
+    //    if (hit1)
+    //    {
+           
+    //        val1 = hit1.point.x;
+    //        print("detect 1 !");
+    //    }
+    //    if (hit2)
+    //    {
+            
+    //        val2 = hit2.point.x;
+    //        print("detect 2 !");
+    //    }
+    //    if (hit3)
+    //    {
+           
+    //        val3 = hit3.point.x;
+    //        print("detect 3 !");
+    //    }
+
+
+
+    //    if (hit4)
+    //    {
+            
+    //        val4 = hit4.point.x;
+    //        print("detect 4 !");
+    //    }
+    //    if (hit5)
+    //    {
+            
+    //        val5 = hit5.point.x;
+    //        print("detect 5 !");
+    //    }
+    //    if (hit6)
+    //    {
+    //        val6 = hit6.point.x;
+    //        print("detect 6 !");
+    //    }
+
+
+    //    if (hit7)
+    //    {
+    //        val7 = hit7.point.y;
+    //        print("detect 7 !");
+    //    }
+    //    if (hit8)
+    //    {
+    //        val8 = hit8.point.y;
+    //        print("detect 8 !");
+    //    }
+    //    if (hit9)
+    //    {
+    //        val9 = hit9.point.y;
+    //        print("detect 9 !");
+    //    }
+
+
+    //    if (hit10)
+    //    {
+    //        val10 = hit10.point.y;
+    //        print("detect 10 !");
+    //    }
+    //    if (hit11)
+    //    {
+    //        val11 = hit11.point.y;
+    //        print("detect 11 !");
+    //    }
+    //    if (hit12)
+    //    {
+    //        val12 = hit12.point.y;
+    //        print("detect 12 !");
+    //    }
+
+    //    float minX = Mathf.Max(val1, val2, val3) + width + 0.35f;
+    //    float maxX = Mathf.Min(val4, val5, val6) - width - 0.35f;
+    //    float minY = Mathf.Max(val7, val8, val9) + height + 0.35f;
+    //    float maxY = Mathf.Min(val10, val11, val12) - height - 0.35f;
+
+
+    //    float X = Mathf.Clamp(this.transform.position.x, minX , maxX );
+    //    float Y = Mathf.Clamp(this.transform.position.y, minY, maxY );
+
+    //    this.transform.position = new Vector3(X, Y, -1);
+        
+        
+
+    //}
+
+
+    
+    
+    
     public bool getdessous()
     {
         return dessous;
@@ -106,28 +325,45 @@ public class character : MonoBehaviour {
         return droite;
     }
 
-    void gravity()
+    public void gravity()
     {
-        if (!dessus)
+        if (!landed)
         {
-            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - time * time * gpower / 2, this.transform.position.z);
-            time += Time.deltaTime;
-        } else
+            VerticalSpeed = VerticalSpeed - g * Time.deltaTime;
+            print(" apply gravity !");
+        }
+        print("gravity");
+    }
+
+    public void jump()
+    {
+        if (landed)
         {
-            time = 0;
+            landed = false;
+            VerticalSpeed = jumpPower;
+            print("jump !");
+
         }
         
+    }
+
+    public float getVerticalSpeed()
+    {
+        return VerticalSpeed;
     }
     // Use this for initialization
     void Start () {
         width = this.gameObject.GetComponent<Collider2D>().bounds.size.x/2;
         height = this.gameObject.GetComponent<Collider2D>().bounds.size.y/2;
-        dessus = false;
         time = 0;
+        distance = 15f;
+        landed = false;
+        VerticalSpeed = 0;
 	}
 	
 	// Update is called once per frame
 	void Update () {
         gravity();
+        print(landed);
 	}
 }
--- Assets/Scripts/input_controller.cs
+++ Assets/Scripts/input_controller.cs
@@ -9,7 +9,7 @@ public class input_controller : MonoBehaviour {
         if (Input.GetButton("Fire1"))
         {
             //this.gameObject.GetComponent<Renderer>().material.color = Color.green;
-            this.GetComponent<character>().jump();
+           
         } else if (Input.GetButton("Fire2"))
         {
             this.gameObject.GetComponent<Renderer>().material.color = Color.red;
@@ -25,32 +25,45 @@ public class input_controller : MonoBehaviour {
     void inputMoveJoystick()
     {
         float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
-
-        if (((h < 0) && (!this.GetComponent<character>().getdroite())) || ((h > 0) && (!this.GetComponent<character>().getgauche())))
+        //float v = Input.GetAxis("Vertical");
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            this.gameObject.transform.position = this.gameObject.transform.position + new Vector3(h * Time.deltaTime * 5, 0, 0);
+            this.GetComponent<character>().jump();
+            this.GetComponent<character>().landed = false;
         }
-        if (((v < 0) && (!this.GetComponent<character>().getdessus())) || ((v > 0) && (!this.GetComponent<character>().getdessous())))
+
+        //if (((h < 0) && (!this.GetComponent<character>().getdroite())) || ((h > 0) && (!this.GetComponent<character>().getgauche())))
+        //{
+        //    this.gameObject.transform.position = this.gameObject.transform.position + new Vector3(h * Time.deltaTime * 5, 0, 0);
+        //}
+        //if (((v < 0) && (!this.GetComponent<character>().getdessus())) || ((v > 0) && (!this.GetComponent<character>().getdessous())))
+        //{
+        //    this.gameObject.transform.position = this.gameObject.transform.position + new Vector3(0, v * Time.deltaTime * 5, 0);
+        //}
+        float minX = this.gameObject.GetComponent<character>().CollisionLeft();
+        float maxX = this.gameObject.GetComponent<character>().CollisionRight();
+        float minY = this.gameObject.GetComponent<character>().CollisionDown();
+        float maxY = this.gameObject.GetComponent<character>().CollisionUp();
+        if (h != 0)
         {
-            this.gameObject.transform.position = this.gameObject.transform.position + new Vector3(0, v * Time.deltaTime * 5, 0);
+            print("input");
+            Vector3 pos = this.gameObject.transform.position + new Vector3(h * Time.deltaTime * 5, this.gameObject.GetComponent<character>().getVerticalSpeed(), 0);
+            pos.x = Mathf.Clamp(pos.x, minX, maxX);
+            pos.y = Mathf.Clamp(pos.y, minY, maxY);
+            this.gameObject.transform.position = pos;
+        } else
+        {
+            Vector3 pos = this.gameObject.transform.position;
+            pos.x = Mathf.Clamp(this.gameObject.transform.position.x, minX, maxX);
+            pos.y = Mathf.Clamp(this.gameObject.transform.position.y + this.gameObject.GetComponent<character>().getVerticalSpeed(), minY, maxY);
+            this.gameObject.transform.position = pos;
+            
         }
-        //if (h != 0 || v != 0)
-        //{
-        //    Vector3 pos = this.gameObject.transform.position + new Vector3(h * Time.deltaTime * 5, v * Time.deltaTime * 5, 0);
-        //    //pos.x = Mathf.Clamp(pos.x, -4, 4);
-        //    //pos.y = Mathf.Clamp(pos.y, -4, 4);
-        //    this.gameObject.transform.position = pos;
-        //}
-        //else
-        //{
-
-
-        //}
+       
     }
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
